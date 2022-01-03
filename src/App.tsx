967a046cc1603e01875cb0a961e019f81800293c
paschoal <jaqueline.paschoal@scaffoldeducation.com.br>
2022-01-03 18:36:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {GlobalStyle} from '../src/styles/global'
+
+export function App() {
+  return (
+    <div className="App">
+     Teste
+     <GlobalStyle/>
+    </div>
+  );
+}
